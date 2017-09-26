e12ae02e2efb19e49bf1060b3ddf22c1f7bd2264
Myroslava Romaniuk <romaniuk@ucu.edu.ua>
2017-09-26 21:16:57 +0300

    more changes added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,17 +3,17 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.awt.*;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         String url = "https://rozetka.com.ua/ua/memory-cards/c80044/";
         parseCategory(url);
+        //parseReviewsPage("https://rozetka.com.ua/ua/kingston_sdc10g2_32gb/p5846073/comments/page=2/");
     }
 
     public static void parseCategory(String url) throws IOException {
@@ -43,11 +43,11 @@ public class Main {
             num = Integer.parseInt(nums.last().text());
         } else num = 0;
 
-        String[] sentiments = new String[num];
+        ArrayList<Review> sentiments = new ArrayList<>();
 
         for(int i=0; i<num; ++i) {
             String pg = url + "page=" + Integer.toString(i+1) + "/";
-            sentiments += parseReviewsPage(pg);
+            sentiments.addAll((parseReviewsPage(pg)));
         }
 
         String filename = "data/" + url.split("/")[4] + ".csv";
@@ -58,20 +58,24 @@ public class Main {
         pw.close();
     }
 
-    public static void parseReviewsPage(String url) throws IOException {
+    public static ArrayList<Review> parseReviewsPage(String url) throws IOException {
         Document doc = Jsoup.connect(url).get();
         Elements reviews = doc.select("article.pp-review-i");
-        String[] sentiments = new String[reviews.size()];
+        //String[] sentiments = new String[reviews.size()];
+        ArrayList<Review> sentiments = new ArrayList<>();
 
         for(Element review: reviews) {
             Elements star = review.select("span.g-rating-stars-i");
             Elements text = review.select("div.pp-review-text");
-            if(star>0) {
-                Elements texts = text.select("div.pp-review-text-i");
-                sentiments.add((star.select("content"), Integer.parseInt(texts.first().text()));
+
+            if(star.size()>0) {
+                //System.out.println("star " + star.get(0).attr("content"));
+                //System.out.println("text " + text.text());
+                sentiments.add(new Review(text.text(), Integer.parseInt(star.get(0).attr("content"))));
+                //Elements texts = text.select("div.pp-review-text-i");
+                //sentiments.addAll((star.select("content"), Integer.parseInt(texts.first().text()));
             }
         }
-
-        return sentiments[];
+        return(sentiments);
     }
 }
---
+++ src/main/java/Review.java
@@ -0,0 +1,9 @@
+public class Review {
+    private String text;
+    private int sentiment;
+
+    public Review(String text, int sentiment){
+        this.text = text;
+        this.sentiment = sentiment;
+    }
+}
